a226a01dd459f8ebd211b52f3ef751f0906142ab
ebbarickard <ebbarickard@localhost>
2020-10-01 18:14:17 +0200

    Sheet

--- xl/src/expr/Sheet.java
+++ xl/src/expr/Sheet.java
@@ -1,7 +1,21 @@
 
-public class Sheet implements Environment {
+public class Sheet implements Environment implements Observable {
+	
+	private TreeMap<Entry<String, Expr>> sheet;
 	
 	public double value(String name) {
 
+	}
+	
+	public writeCell(String text, Expr) {
+		
+	}
+	
+	public Expr get(String text) {
+		
+	}
+	
+	private double Calculate() { //??
+		
 	}
 }
