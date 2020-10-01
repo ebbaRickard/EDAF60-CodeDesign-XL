151e75dba6a0f8911c5cabf88c72b847d3db31ec
ebbarickard <ebbarickard@localhost>
2020-10-01 22:13:42 +0200

    Metoder i sheet

--- xl/src/expr/Sheet.java
+++ xl/src/expr/Sheet.java
@@ -1,21 +1,29 @@
+package expr;
 
-public class Sheet implements Environment implements Observable {
+import java.util.TreeMap;
+import java.util.Observable;
+
+public class Sheet extends Observable implements Environment {
+
+	private TreeMap<String, Expr> sheet;
 	
-	private TreeMap<Entry<String, Expr>> sheet;
-	
+	public Sheet() {
+		sheet = new TreeMap<>(); 
+	}
+
 	public double value(String name) {
+		return 0;
+	}
+
+	public void writeCell(String text, Expr expression) {
 
 	}
-	
-	public writeCell(String text, Expr) {
-		
-	}
-	
+
 	public Expr get(String text) {
-		
+		return null;
 	}
-	
-	private double Calculate() { //??
-		
+
+	private double Calculate() { // ??
+		return 0;
 	}
 }
